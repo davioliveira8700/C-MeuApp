4911f7a8184b9e709e40cd9c4e38f643a2924775
Davi de Oliveira <odavi278@gmail.com>
2023-12-28 00:59:21 -0300

    Primeiro commit curso de fundamento c# balta

---
+++ MeuApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MeuApp
+{
+    class program 
+    {
+        static void Main(string[] args)
+        {
+            var texto = "testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
